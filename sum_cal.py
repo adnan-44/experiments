678e51aec17271e9691cf5a1d667ecfcd42ce9fa
Your Name <you@example.com>
2020-09-24 01:32:41 +0530

    improvemnt in code

--- sum_cal.py
+++ sum_cal.py
@@ -1,10 +1,11 @@
 print('This is first Pyhton program')
 i = 0
 while(True):
+	print('Enter a number')
 	num = input()
 	if num == 'no':
 		print('done ')
-		print("Task executed.....')
+		print('Total sum is',i)
+		print('Task executed.....')
 		exit()
-	i = i+1
-	print('Executed for the',i,' time')
+	i = i+int(num)
