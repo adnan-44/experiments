ad5af64557b4edd556200038dd014102d93d4b6b
adnan-44 <mohd.adnan0405@gmail.com>
2020-10-03 04:19:20 +0530

    Fixed typo in spam2.py script

--- spam2.py
+++ spam2.py
@@ -6,6 +6,6 @@ for word in f:
 	pyautogui.press("enter")
 f.close()
 f1 = open("hello1.txt",'r')
-for word in f:
+for word in f1:
         pyautogui.typewrite(word)
         pyautogui.press("enter")
