60190e21ef6b988b8de63b0af37ad6cdece6bb5a
adnan-44 <mohd.adnan0405.com>
2020-10-14 15:32:31 +0530

    Updated py file

--- spamm.py
+++ spamm.py
@@ -1,9 +1,10 @@
 import pyautogui, time
 time.sleep(5)
 #f = open("hello.txt",'r')
-num = 100
+num = 3
 #for word in f:
 while num > 0:
-	pyautogui.typewrite('You do not have time, wait')
+	pyautogui.typewrite('kumar')
 	pyautogui.press("enter")
 	num = num - 1
+	#time.sleep(0.9)
