d5d6ff34299912c8b169489578d7e3a7fbcb4c0c
adnan-44 <mohd.adnan0405@gmail.com>
2021-01-25 11:50:59 +0530

    JAVA: add a String related class

---
+++ JAVA/StringQ.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StringQ{
+  public static void main(String [] args)
+  {
+    String str1 = "FACE";
+    StringBuffer str2 =  new StringBuffer(str1);
+    System.out.println(str1.equals(str2));
+  }
+}
