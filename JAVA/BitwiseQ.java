ab3de0a2729a7e1d45ea17b1d7d00a71838734df
adnan-44 <mohd.adnan0405@gmail.com>
2021-01-25 11:55:55 +0530

    JAVA: add a BitwiseQ file
    This will show how a bitwise operator work on int

---
+++ JAVA/BitwiseQ.java
@@ -0,0 +1,8 @@
+  import java.util.*;
+  class BitwiseQ{
+    public static void main(String[] args){
+      int var1 = 42;
+      int var2 = ~var1;
+      System.out.println(var1+" "+var2);
+    }
+  }
