ab77d473bbca9ffd471620050ceb9f90f5028f67
adnan-44 <mohd.adnan0405@gmail.com>
2021-01-30 14:58:03 +0530

    py: Added SumList file
    This code will calculate sum of elements in a list

---
+++ List/SumList.py
@@ -0,0 +1,9 @@
+#this code will calclute sum of all elements in list with loop
+l = [12,45,23,34,54,23,45]
+
+#initialize sum variable with 0
+sum = 0
+for i in l:
+   sum = sum+i
+
+print('Sum is :',sum)
