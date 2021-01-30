3e1ad8acdf30b723fde6bc54dbf90b1bf1fbd2f1
adnan-44 <mohd.adnan0405@gmail.com>
2021-01-30 15:12:16 +0530

    py: Added a BasicOperation file
    This code can do some basic operations:
    1. Sum of two numbers
    2. Area of cube
    3. Whether a number is even or odd

---
+++ BasicOperation.py
@@ -0,0 +1,16 @@
+#1. Sum of two numbers
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+print('Sum ',a+b)
+
+#2 Calculate Area of cube
+a = int(input('\nEnter side of Cube: '))
+print('Area of Cube',6*a*a)
+
+#3 Check even or odd
+a = int(input('\nEnter a number: '))
+if a%2==0:
+   print(a,' is Even.')
+else:
+   print(a,' is Odd.')
+
