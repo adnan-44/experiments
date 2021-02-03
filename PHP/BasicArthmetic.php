02f72eacabdcab678b62cabdc139efc826bb41ed
adnan-44 <mohd.adnan0405@gmail.com>
2021-02-03 20:38:04 +0530

    php: added BasicArthmetic file
    This code will do basic arthmetic operations, and print them.

---
+++ PHP/BasicArthmetic.php
@@ -0,0 +1,30 @@
+<?php
+/* This code will do basic arthmetic operations and print them */
+
+//declare variables
+  $a = 45;
+  $b = 77;
+
+  echo "\$a = $a, \$b = $b <br><br>";
+  $c = $a + $b;
+  echo "Addtion Operation, \$a + \$b = $c <br/><br>";
+
+  $c = $a - $b;
+  echo "Substraction Operation, \$a - \$b = $c <br/><br>";
+
+  $c = $a * $b;
+  echo "Multiplication Operation, \$a * \$b = $c <br/><br>";
+
+  $c = $a / $b;
+  echo "Division Operation, \$a / \$b = $c <br/><br>";
+
+  $c = $a % $b;
+  echo "Modulus Operation, \$a % \$b = $c <br/><br>";
+
+  $c = $a++;
+  echo "Increment Operation, \$a++ = $c <br/><br>";
+
+  $c = $a--;
+  echo "Decrement Operation, \$a-- = $c <br/><br>";
+
+?>
